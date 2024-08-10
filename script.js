cd5bf9fea8aec23c216aacf896eb241b1839ac3e
avi <gajai.avinash@gmail.com>
2024-08-10 02:49:35 -0400

    created a button that opens a form and creates book object

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-   this.title = title;
-   this.author = author;
-   this.pages = pages;
-   this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 
-   this.getBookInfo = function() {
-       return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'already read' : 'not read yet'}`;
-     };
-   }
-function addBookToLibrary(book) {
-   myLibrary.push(book);
+  this.getBookInfo = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "already read" : "not read yet"
+    }`;
+  };
 }
-
-
-const book1 = new Book('Count of Monte Cristo', 'Alexander Dumas', 1000, true);
-const book2 = new Book('Three Musketeers', 'Alexander Dumas', 1000, true);
-const book3 = new Book('Geratia', 'Alexander Dumas', 1000, true);
-const book4 = new Book('Gestapo', 'Alexander Dumas', 1000, true);
-const book5 = new Book('Lord of the Rings', 'Alexander Dumas', 1000, true);
-
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-addBookToLibrary(book4);
-
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
 function displayBook() {
-   myLibrary.forEach((book) => 
-   console.log(book));
-}
+  const bookList = document.getElementById("bookList");
 
-displayBook();
+  myLibrary.forEach((book, index) => {
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book"); //this adds css class to the div we created above
+    bookElement.setAttribute("data-index", index);
+
+    bookElement.innerHTML = `
+    <h3>${book.title}</h3>
+    <p>Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    <p>Read: ${book.read ? "Yes" : "No"}</p>
+    <button onclick="removeBook(${index})">Remove</button>
+    <button onclick="toggleRead(${index})">Toggle Read</button>
+  `;
+
+    bookList.appendChild(bookElement);
+  });
+}
 
 // button that pops up a form
 function newBookForm() {
-   document.getElementById('form1').style.display = 'block';
+  document.getElementById("form1").style.display = "block";
 }
+
+// submit button
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const form = document.getElementById("form1");
+  const formData = new FormData(form);
+
+  const author = formData.get("authorEntry");
+  const title = formData.get("titleEntry");
+  const pages = formData.get("pageEntry");
+  const read = formData.get("readEntry");
+
+  const newBook = new Book(title, author, pages, read);
+
+  addBookToLibrary(newBook);
+  displayBook();
+}
+
+console.table(myLibrary);
